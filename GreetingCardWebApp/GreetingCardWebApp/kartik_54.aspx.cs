d947769c719f2ff753c401a37495116d80a823ed
ktkakadiya <ktkakadiya@gmail.com>
2016-08-31 23:47:34 +0530

    Version 1 is created.

---
+++ GreetingCardWebApp/GreetingCardWebApp/kartik_54.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GreetingCardWebApp
+{
+    public partial class kartik_54 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == true)
+            {
+                Panel1.BackColor = System.Drawing.Color.FromName(TextBox1.Text);
+                Label7.ForeColor = System.Drawing.Color.FromName(TextBox2.Text);   
+            }
+        }
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList2.SelectedIndex == 0)
+            {
+                TextBox2.Enabled = false; 
+            }
+            else
+            {
+                TextBox2.Enabled = true;   
+            }
+            Panel1.BorderStyle = (BorderStyle)(Int32.Parse(DropDownList2.SelectedItem.Value));
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            Label7.Text = TextBox3.Text;         
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Label7.Font.Name = DropDownList3.SelectedItem.Text;
+        }
+
+        protected void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            Label7.Font.Size = Int32.Parse(TextBox4.Text) + 8;
+        }
+    }
+}
